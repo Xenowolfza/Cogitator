086121711f53c0a91f2632c356aa884a39ff58a0
Xenowolfza <manrusa@gmail.com>
2025-11-01 13:43:47 +0200

    "Fix: updated imports for LangChain 1.x retrieval chain"

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.prompts import ChatPromptTemplate
 
